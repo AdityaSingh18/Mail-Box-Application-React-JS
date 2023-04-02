2f4b3d1bc6a1d906176e721bccac7c6f8fb23e1c
Aditya <aditya18.mail@gmail.com>
2023-04-02 15:47:00 +0530

    Adding Signup and Login

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React from "react";
+import {
+  Routes,
+  redirect,
+  Router,
+  Route,
+  createRoutesFromChildren,
+  createBrowserRouter,
+  RouterProvider,
+  BrowserRouter,
+} from "react-router-dom";
+import Login from "./Component/Pages/Login";
+import "mdb-react-ui-kit/dist/css/mdb.min.css";
+import "../node_modules/react-bootstrap/dist/react-bootstrap";
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
+import HeaderSite from "./Component/UI/HeaderSite";
+import Profile from "./Component/Pages/Profile";
+import Error from "./Component/Pages/Error";
+import UpdateProfile from "./Component/Pages/UpdateProfile";
+import VerifyEmail from "./Component/Pages/VerifyEmail";
+import ForgotPassword from "./Component/Pages/ForgotPassword";
+import './App.css';
+
+
+function App() {
+
+  return (
+   
+    <BrowserRouter>
+    
+      <Routes>
+        <Route path="/" element={<HeaderSite />}>
+          <Route index element={<Login/>}/>
+          <Route path="/login" element={<Login />} />
+          
+          <Route path="/Profile" element={<Profile/>} />
+          <Route path="updateprofile" element={<UpdateProfile/>}/>
+          <Route path="verifyemail" element={<VerifyEmail/>}/>
+          <Route path="forgotpassword" element={<ForgotPassword/>}/>
+          <Route path="*" element={<Error />} />
+        </Route>
+      </Routes>
+     
+    </BrowserRouter>
+
+  );
+}
+export default App;
---
+++ src/Component/store/index.js
@@ -0,0 +1,13 @@
+import { createSlice,configureStore } from "@reduxjs/toolkit";
+import { authSlice } from "./auth";
+
+
+
+const store = configureStore({
+    reducer: {auth:authSlice.reducer}
+})
+
+
+export const authActions = authSlice.actions;
+
+export default store;
